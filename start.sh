a24be226124b15e9404cd1897a5995b69a69edb4
Glitch (para-casa-inclusivo) <none>
2023-08-27 03:10:41 +0000

    🐫🧴 Updated with Glitch

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Activate the virtual environment, if you're using one
+# source ./venv/bin/activate  # Uncomment this line if you're using a virtual environment
+
+# Install the Flask application and requeriments
+pip3 install Flask
+pip3 install python-dotenv
+pip3 install -r requirements.txt
+
+# Run the Flask application
+python3 -m flask run --host=0.0.0.0 --port=3000
