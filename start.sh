cff900f7542c6bbb88af261f731bd1bc05341348
Glitch (para-casa-inclusivo) <none>
2023-11-06 23:41:30 +0000

    Install and run adjustments

--- start.sh
+++ start.sh
@@ -4,7 +4,6 @@
 # source ./venv/bin/activate  # Uncomment this line if you're using a virtual environment
 
 # Install the Flask application and requeriments
-pip3 install Flask
 pip3 install -r requirements.txt
 
 # Run the Flask application
